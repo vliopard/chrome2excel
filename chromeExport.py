fb9b232af561c717faf522f5cd34bd9892487177
Andre Alcantara <andre.alcantara@eldorado.org.br>
2020-04-17 13:43:31 -0300

    Generating first URL List

--- chromeExport.py
+++ chromeExport.py
@@ -1,6 +1,10 @@
 import wx
 import glob
+import htmlSupport
 
+hostname="Hostname"
+url_title="Title"
+url_addr="URL"
 date_added="Date Added"
 date_modified="Date Modified"
 date_visited="Date Visited"
@@ -13,9 +17,9 @@ class urlPanel(wx.Panel):
         main_sizer = wx.BoxSizer(wx.VERTICAL)
         self.row_obj_dict = {}
         self.list_ctrl = wx.ListCtrl(self, size=(-1, 100), style=wx.LC_REPORT | wx.BORDER_SUNKEN)
-        self.list_ctrl.InsertColumn(0, 'Hostname', width=140)
-        self.list_ctrl.InsertColumn(1, 'Title', width=140)
-        self.list_ctrl.InsertColumn(2, 'URL', width=200)
+        self.list_ctrl.InsertColumn(0, hostname, width=140)
+        self.list_ctrl.InsertColumn(1, url_title, width=140)
+        self.list_ctrl.InsertColumn(2, url_addr, width=200)
         main_sizer.Add(self.list_ctrl, 0, wx.ALL | wx.EXPAND, 5)
         edit_button = wx.Button(self, label='Edit')
         edit_button.Bind(wx.EVT_BUTTON, self.on_edit)
@@ -38,21 +42,19 @@ class urlPanel(wx.Panel):
         self.list_ctrl.InsertColumn(1, date_modified, width=140)
         self.list_ctrl.InsertColumn(2, date_visited, width=200)
         self.list_ctrl.InsertColumn(3, original_url, width=200)
-        urls = glob.glob(folder_path + '/*.txt')
-        url_objects = []
         index = 0
-
+        url_objects = []
         url_list = []
-        with open("chrome.txt",encoding='utf-8') as bm:
+        with open(folder_path, encoding='utf-8') as bm:
             for line in bm:
                 url_list.append(line)
 
-        for url in url_list:
-            url_parts = htmlSupport.parseURL(line)
-            self.list_ctrl.InsertItem(index, url_parts[2])
-            self.list_ctrl.SetItem(index, 1, htmlSupport.clean_url(url)
-            self.list_ctrl.SetItem(index, 2, url)
-
+        for url in url_list:            
+            self.list_ctrl.InsertItem(index, "IndexA"+str(index))
+            self.list_ctrl.SetItem(index, 1, "IndexB"+str(index))
+            self.list_ctrl.SetItem(index, 2, "IndexC"+str(index))
+            self.list_ctrl.SetItem(index, 3, url)
+            url_object = { "Hostname":"HN"+str(index), "Title":"TT"+str(index), "URL":"UR"+str(index) }
             url_objects.append(url_object)
             self.row_obj_dict[index] = url_object
             index += 1
@@ -69,29 +71,35 @@ class urlFrame(wx.Frame):
     def create_menu(self):
         menu_bar = wx.MenuBar()
         file_menu = wx.Menu()
-        open_folder_menu_item = file_menu.Append(wx.ID_ANY, 'Open Folder', 'Open a folder with TXTs')
+        open_folder_menu_item = file_menu.Append(wx.ID_ANY, 'Open File', 'Open a text file with URLs')
+        open_account_menu_item = file_menu.Append(wx.ID_ANY, 'Import Account', 'Import Account from Chrome')
         menu_bar.Append(file_menu, '&File')
         self.Bind(event=wx.EVT_MENU, handler=self.on_open_folder, source=open_folder_menu_item)
-        self.SetMenuBar(menu_bar)
+        self.Bind(event=wx.EVT_MENU, handler=self.on_open_folder, source=open_account_menu_item)
+        self.SetMenuBar(menu_bar)       
 
     def on_open_folder(self, event):
-        dlg = wx.DirDialog(self, "Choose a directory:", style=wx.DD_DEFAULT_STYLE)
+        #dlg = wx.DirDialog(self, "Choose a directory:", style=wx.DD_DEFAULT_STYLE)
+        wildcard = "Text file (*.txt)|*.txt"
+        dlg = wx.FileDialog(self, message="Choose a file:", defaultFile="chrome.txt", wildcard=wildcard, style=wx.DD_DEFAULT_STYLE)
         if dlg.ShowModal() == wx.ID_OK:
             self.panel.update_url_listing(dlg.GetPath())
         dlg.Destroy()
 
+    def on_open_account(self, event):
+        pass
 
 class EditDialog(wx.Dialog):    
     def __init__(self, url):
-        date_visited = f'Editing "{url.tag.date_visited}"'
+        date_visited = f'Editing "{url.date_visited}"'
         super().__init__(parent=None, title=date_visited)        
         self.url = url        
         self.main_sizer = wx.BoxSizer(wx.VERTICAL)        
-        self.date_added = wx.TextCtrl(self, value=self.url.tag.date_added)
+        self.date_added = wx.TextCtrl(self, value=self.url.date_added)
         self.add_widgets(date_added, self.date_added)
-        self.date_modified = wx.TextCtrl(self, value=self.url.tag.date_modified)
+        self.date_modified = wx.TextCtrl(self, value=self.url.date_modified)
         self.add_widgets(date_modified, self.date_modified)
-        self.date_visited = wx.TextCtrl(self, value=self.url.tag.date_visited)
+        self.date_visited = wx.TextCtrl(self, value=self.url.date_visited)
         self.add_widgets(date_visited, self.date_visited)
         btn_sizer = wx.BoxSizer()
         save_btn = wx.Button(self, label='Save')
@@ -109,10 +117,10 @@ class EditDialog(wx.Dialog):
         self.main_sizer.Add(row_sizer, 0, wx.EXPAND)
 
     def on_save(self, event):
-        self.url.tag.date_added = self.date_added.GetValue()
-        self.url.tag.date_modified = self.date_modified.GetValue()
-        self.url.tag.date_visited = self.date_visited.GetValue()
-        self.url.tag.save()
+        self.url.date_added = self.date_added.GetValue()
+        self.url.date_modified = self.date_modified.GetValue()
+        self.url.date_visited = self.date_visited.GetValue()
+        self.url.save()
         self.Close()
 
 
